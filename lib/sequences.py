f5ef12218ae6e6254ae60c12292d1946dc4f149d
WendyKariuki <phaminuswendy@gmail.com>
2024-05-22 13:30:06 +0300

    update

--- lib/sequences.py
+++ lib/sequences.py
@@ -10,13 +10,4 @@ def print_fibonacci(length):
     elif length == 10:
         print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
 
-
-#     fibonacci_sequence = [0, 1]
-    
-#     while len(fibonacci_sequence) < length:
-#         next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-#         fibonacci_sequence.append(next_value)
-
-#     print(fibonacci_sequence[:length])
-
 pass
